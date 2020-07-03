da633b9d1abd7470fd788603688b83b58240b1de
Martin Lan <martin,lan.mora@gmail.com>
2020-07-02 21:41:24 -0300

    Lo mismo, mas prolijo

--- views/app.js
+++
@@ -1,7 +0,0 @@
-const express =require ("express")
-const app = express ();
-app.use (express.static("public"));
-
-app.listen (4000, function(){ console.log("Te oigo")});
-
-app.get ("/", function (req,res){ res.sendFile(__dirname + "/index.html")})
