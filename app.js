64c30deffa480cb84f2c3d0c44e512b8ee78f7e9
Martin Lan <martin,lan.mora@gmail.com>
2020-07-03 17:53:29 -0300

    Con CSS

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express =require ("express")
 const app = express ();
 app.use (express.static("public"));
 
-app.listen (4000, function(){ console.log("Te oigo")});
+app.listen (4100, function(){ console.log("Te oigo")});
 
 app.get ("/", function (req,res){ res.sendFile(__dirname + "/views/index.html");})
